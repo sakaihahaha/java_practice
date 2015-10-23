4561b9d6c3a0cd843926759faca40eb3c0ba620f
sakai.yuu <hikohikohohehohe@gmail.com>
2015-10-23 11:26:19 +0900

    java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,23 @@
-public class Hello{
-	public static void main(String[] args){
-		System.out.println("Hello!!! year!!!World!!!!");
+package hey;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num = new java.util.Random().nextInt(5);
+		switch(num){
+			case 1;
+			case 2;
+				System.out.println("大吉");
+				break;
+			case 3;
+				System.out.println("中吉");
+				break;
+			case 4;
+			case 5;
+				System.out.println("小吉");
+				break;
+		}
 	}
+
 }
